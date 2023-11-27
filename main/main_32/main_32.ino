603767231230fcd7924917d6e891369570d51765
MustafaaYilmas <mustafa.yilmaz@netcad.com>
2023-11-27 18:02:32 +0300

    Update main_32.ino

--- main/main_32/main_32.ino
+++ main/main_32/main_32.ino
@@ -1,38 +1,38 @@
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
 #include <ThingsBoard.h>
 #include <Arduino_MQTT_Client.h>
 #include <WiFiClient.h>
 #include <ArduinoJson.h>
 #include "Adafruit_SHT4x.h"
 
+// WiFi network credentials
 #define WIFI_AP_NAME        "Vodafonenet_Wifi_8047"
 #define WIFI_PASSWORD       "U5DerrGGDUEz"
+
+// Credentials for the ThingsBoard platform
 #define TOKEN               "pHYZ7VkKvoxTOyy6LDc1"
 #define THINGSBOARD_SERVER  "demo.thingsboard.io"
-
-constexpr uint16_t MAX_MESSAGE_SIZE = 128;
 constexpr uint16_t THINGSBOARD_PORT = 1883;
 
-
+// Defining objects necessary for MQTT and ThingsBoard on ESP32
 WiFiClient espClient;
 Arduino_MQTT_Client mqttClient(espClient);
 ThingsBoard tb(mqttClient, MAX_MESSAGE_SIZE);
 int status = WL_IDLE_STATUS;
 
-
+// Creating an instance of the Adafruit SHT4x sensor
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 
-
+// Structure to hold data from the SHT4x sensor
 struct SHT40Data
 {
   float temperature;
   float humidity;
 };
 
-
+// Function to initialize WiFi connection
 void InitWiFi()
 {
+  // Connecting to the WiFi network
   Serial.println("Connecting to AP ...");
   WiFi.begin(WIFI_AP_NAME, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
@@ -42,6 +42,7 @@ void InitWiFi()
   Serial.println("Connected to AP");
 }
 
+// Function to reconnect to WiFi if the connection is lost
 void reconnect() {
   status = WiFi.status();
   if (status != WL_CONNECTED) {
@@ -54,6 +55,7 @@ void reconnect() {
   }
 }
 
+// Initializing the SHT4x sensor
 void shtBegin(){
     Serial.println("Adafruit SHT4x test");
   if (! sht4.begin()) {
@@ -69,18 +71,18 @@ void shtBegin(){
   sht4.setHeater(SHT4X_NO_HEATER);
 }
 
+// Function to read temperature and humidity from the SHT4x sensor
 SHT40Data readTemperatureAndHumidity(){
   sensors_event_t humidity, temp;
   sht4.getEvent(&humidity, &temp);
-  Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
-  Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+  //Serial.print("Temperature: "); Serial.print(temp.temperature); Serial.println(" degrees C");
+  //Serial.print("Humidity: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
 
   SHT40Data data;
   data.temperature = temp.temperature;
   data.humidity = humidity.relative_humidity;
   return data;
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -105,11 +107,11 @@ void loop() {
 
   SHT40Data returnedData = readTemperatureAndHumidity();
 
+  // Sending temperature and humidity data to ThingsBoard
   tb.sendTelemetryData("temperature", returnedData.temperature);
   tb.sendTelemetryData("humidity", returnedData.humidity);
 
-  Serial.println("Telemetry data sent.");
+  //Serial.println("Telemetry data sent.");
 
-  // Process messages
   tb.loop();
 }
